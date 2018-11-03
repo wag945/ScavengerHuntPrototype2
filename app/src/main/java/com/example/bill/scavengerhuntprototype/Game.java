dcdaa37e005537d6a2985cc40ae9cf66b217c51c
William Zerega <Bill@Williams-MacBook-Pro-8.local>
2018-11-03 08:20:03 -0400

    added scavenge item class, and implemented 5 items in start game class

--- app/src/main/java/com/example/bill/scavengerhuntprototype/Game.java
+++ app/src/main/java/com/example/bill/scavengerhuntprototype/Game.java
@@ -1,4 +1,8 @@
 package com.example.bill.scavengerhuntprototype;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.Vector;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -11,6 +15,13 @@ public class Game {
     private Timer mTimer;
     private GameTimer mGameTimer;
     //Need a timer task to run the timer
+
+    private String[] scavengeItemsBank = new String[]{"Red Sign", "Yellow Sign", "2 Right White Shoes", "2 Left Black Shoes",
+        "Animal Statue", "Person Statue", "Flag", "Green Ball", "A Clock", "Person in Uniform", "Out of State License Plate",
+        "A Book"};
+    //creating a copy of the scavenge bank so that items can be removed as they are selected
+    private List<String> scavengeItemsBankList = new ArrayList<>(Arrays.asList(scavengeItemsBank));
+    private ArrayList<ScavengeItem> mScavengeList;
 
     enum GameState {
         NOT_STARTED,
@@ -50,6 +61,19 @@ public class Game {
         //Start a timer to run immediately and repeat every second
         mTimer.schedule(mGameTimer,0,1000);
         mGameState = GameState.IN_PROGRESS;
+
+        //create 5 Scavenge items for the teams to find
+        for(int i = 0; i < 5; i++){
+            Random random = new Random();
+            int index = random.nextInt(scavengeItemsBankList.size());
+            String scavengeItemString = scavengeItemsBankList.get(index);
+            //removing scavenge item from bank list copy to prevent doubles of same item
+            scavengeItemsBankList.remove(index);
+            ScavengeItem scavengeItem = new ScavengeItem(scavengeItemString);
+
+            mScavengeList.add(i, scavengeItem);
+
+        }
     }
 
     public void stopGame() {
